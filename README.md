2491cb05ed124b1b3f40d8f1650851a4aaf91997
Janiele <njaniele@yahoo.com>
2021-01-23 18:42:41 -0300

    pagina criada

---
+++ README.md
@@ -0,0 +1,37 @@
+<h1 align="center"> Threads.Dev <h1>
+
+<h1> <img src="public/time.png" alt=""> <h1>
+
+
+# Indice
+- [Sobre](#-sobre)
+- [Tecnologias utilizadas](#-tecnologias-utilizadas)
+- [Como baixar o projeto](#-como-baixar-o-projeto)
+
+---
+
+# 📋 Sobre
+Página de apresentação dos desenvolvedores e projetos da threads.dev
+
+---
+
+## 🚀 Tecnologias utilizadas 
+
+O projeto foi desenvolvido utilizando as seguintes tecnologias:
+
+- [HTML](https://www.w3schools.com/html/)
+- [CSS](https://www.w3schools.com/css/default.asp)
+- [Javascript](https://www.w3schools.com/js/default.asp)
+
+---
+
+## 📁 Como baixar o projeto
+```bash
+
+    #Clonar repositório
+    $ git clone https://github.com/JayGiG/threads
+
+```
+
+---
+Desenvolvido por 👩‍💻 Janiele Nogueira
---
+++ data.js
@@ -0,0 +1,31 @@
+module.exports = [
+    {
+        id:"junior",
+        url: "https://github.com/oojuniin",
+        imagem: "img/jonhatan.jpg",
+        titulo1: "Jonhatan ",
+        titulo2: "- Backend | Infra",
+        cor_destaque_titulo: "conteudo_destaque_starter",
+        
+    },
+
+    {
+        id:"janiele",
+        url: "https://github.com/JayGiG",
+        imagem: "img/janiele.png",
+        titulo1: "Janiele ",
+        titulo2: " - Frontend | UX Design",
+        cor_destaque_titulo: "conteudo_destaque_launchbase",
+       
+    },
+
+    {
+        id:"daniel",
+        url:"https://github.com/davdwd",
+        imagem: "img/daniel.jpg",
+        titulo1: "Daniel ",
+        titulo2: " - BD | Marketing",
+        cor_destaque_titulo: "conteudo_destaque_gostack",
+        
+    }
+]
